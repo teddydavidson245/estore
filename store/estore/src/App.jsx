cabfe9cc2c9cb853e610267489335e8fe453dfdc
teddydavidson245 <67019765+teddydavidson245@users.noreply.github.com>
2024-02-09 17:46:32 -0600

    updated index.css

--- store/estore/src/App.jsx
+++ store/estore/src/App.jsx
@@ -1,35 +1,126 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/layouts.css"
+import Navbar from "./components/Header.jsx";
+import ShoppingCart from './components/ShoppingCart.jsx';
+import AuthForm from './components/AuthForm.jsx';
+import ContactUsFooter from './components/ContactUs.jsx';
+import SocialMediaFooter from './components/Footer.jsx';
+import StripeContainer from './components/StripeContainer.jsx';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from './components/HomePage.jsx';
+import ProductDetails from "./components/ProductDetails.jsx";
+import ProductList from "./components/ProductList.jsx";
+import Header from "./components/Header.jsx"
+// import Login from "./components/Login.jsx";
+// import Register from "./components/Register.jsx";
+import "./styles/layouts.css";
+
+
+let cartFromLocalStorage = [];
+try {
+  const cartData = localStorage.getItem('cart');
+  if (cartData) {
+    cartFromLocalStorage = JSON.parse(cartData);
+  }
+
+} catch (error) {
+  console.error('Error parsing JSON data from localStorage', error);
+}
+
+// const cartFromLocalStorage = JSON.parse(localStorage.getItem('cart') || [] )
+
+// }catch (erro) {console.error('Error parsing data', error);
+// }
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const appStyle = {
+  //   backgroundImage: `url(${background1})`, // Apply background image
+  //   backgroundSize: 'cover', // Adjust background size as needed
+  //   backgroundRepeat: 'no-repeat', // Prevent background image from repeating
+  // };
+
+  const [cart, setCart] = useState(cartFromLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+
+// Function to add a product to the cart
+ const addToCart = (product) => {
+  const existingCartItem = cart.find((item) => item.id === product.id);
+
+  if (existingCartItem) {
+    const updatedCart = cart.map((item) =>
+      item.id === product.id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+    );
+    setCart(updatedCart);
+  } else {
+    setCart([...cart, { ...product, quantity: 1 }]);
+  }
+};
+// Function to remove a product from the cart
+const removeFromCart = (productId) => {
+  const updatedCart = cart.filter((item) => item.id !== productId);
+  setCart(updatedCart);
+};
+  // Calculate the cart count based on the cart state
+  const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Router>
+    <div >
+      <div><Navbar cartCount={cartCount}/></div>
+      
+      <Routes> 
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+        {/* <Route path="/productlist" element={<ProductList/>}/> */}
+        <Route path="/productlist/:id" element= {<ProductDetails 
+          cart={cart}
+          setCart={setCart}
+          cartCount={cartCount}
+          removeFromCart={removeFromCart}/>}/>
+        <Route path="/register" element={<AuthForm isRegistering={true} />} />
+            <Route path="/login" element={<AuthForm isRegistering={false} />} />
+            
+        <Route
+            path="/productlist"
+             element= {<ProductList
+                cart={cart}
+                setCart={setCart}
+                cartCount={cartCount}
+                removeFromCart={removeFromCart}
+              />
+            }
+          />
+          {/* <Route
+          path='/productlist'
+          render={(props) => <ProductList {...addToCart} isAuthed={true} />}
+        /> */}
+          <Route path="/cart" element={
+            <ShoppingCart cart={cart} removeFromCart={removeFromCart} updateQuantity={setCart}  />}/>
+
+          <Route path="/checkout" element={
+            <StripeContainer cart={cart} />} />
+            
+            
+          
+      </Routes>
+      <footer id='footer'>
+        <ContactUsFooter/>
+      <SocialMediaFooter/>
+      
+      </footer>
+    </div>
+    </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
